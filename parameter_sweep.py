edda29dd20629e3c47c5e76d2e94ffa024732fed
Anand Sampat <asampat3090@gmail.com>
2013-11-24 15:15:23 -0800

    added feature as arg to main function + added basic parameter sweep

--- parameter_sweep.py
+++ parameter_sweep.py
@@ -1,3 +1,9 @@
-import subprocess
+import os,subprocess
 
-subprocess.call("c")
+featureExtractors = ['bigram','trigram','fourgram']
+# Sweep over different feature extractors 
+for feature in featureExtractors: 
+    # Sweep over different trainingIters
+    for i in range(1,5):
+        subprocess.call(['python','main.py','0','1000','200',str(i),'100','0',feature])
+    
