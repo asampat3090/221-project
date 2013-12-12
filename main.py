9f607fbc0129ee7ca84fcf7c9aa11cf35169d9f1
Anand Sampat <asampat3090@gmail.com>
2013-12-12 02:06:15 -0800

    changed some parameter sweeps - minor

--- main.py
+++ main.py
@@ -100,6 +100,20 @@ def main():
     print "Extract features: ", thisTime - lastTime, ' s'
     lastTime = thisTime
     
+    # CODE TO CALCULATE TOTAL NUMBER OF FEATURES (FOR POSTER)
+    # Calculate all of the features (not just those from the example)  
+    allFeatures = Counter()
+    trainArray = []
+    trainArray = [fs for (fs, label) in trainFeaturesAndLabels]
+    for trainSet in trainArray :
+        allFeatures.update(trainSet)
+    testArray = []
+    testArray = [fs for (fs,label) in testFeaturesAndLabels]
+    for testSet in testArray :
+        allFeatures.update(testSet)
+    
+    print "THE TOTAL NUMBER OF FEATURES IS: %d" % len(allFeatures)
+    
     #Feature Selection
     if numFeatures != 0:
         bestFeatures = featureSelection(trainFeaturesAndLabels, labels, numFeatures)
