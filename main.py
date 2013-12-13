f3241ff0f9043a50c02e05fc64ac6ff3e114bc6d
mschoendorf <megan2@stanford.edu>
2013-12-12 20:49:50 -0800

    no clue

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import glob, os, random
+import glob, os, random, nb
 import sys, time
 from Classifier import *
 from features import *
 from loadExamples import *
 from feature_selector import *
-from nb import *
 
 """
 ARGUMENTS:
@@ -18,6 +17,7 @@ B:                            the regularization parameter, if set to something 
 'unigram','bigram','trigram' or 'fourgram' which feature extractor to use.
 numFeatures:           how many features do we want to use? - if 0 then just take all.
 'nb' or 'gd':              Naive Bayes or Gradient Descents
+'log', 'hinge', 'perceptron'   which loss function to use with gradient descent.
 
 """
 
@@ -27,7 +27,8 @@ def main():
     #If arguments were given, read them in:
     #Arguments are: numTrain, numTest, trainingIter, alpha, regularization constant,
     #artist or genre, unigram/bigram/trigram/fourgram, number of features, nb or gd
-    if len(sys.argv) == 11:
+    #log hinge or perceptron
+    if len(sys.argv) == 12:
         numLabels = int(sys.argv[1])
         numTrainSongs = int(sys.argv[2])
         numTestSongs = int(sys.argv[3])
@@ -38,10 +39,13 @@ def main():
         featureExtractor = sys.argv[8]
         numFeatures = int(sys.argv[9])
         useNB = True if sys.argv[10] == 'nb' else False
+        if sys.argv[11] == 'perceptron': lossFunc = perceptronH
+        elif sys.argv[11] == 'hinge': lossFunc = hingeH
+        else: lossFunc = logisticH
         
     else:
-        print "Main function takes 10 arguments: numLabels, numTrainSongs, numTestSongs, trainingIters, alpha, B, artist/genre uni/bi/tri/fourgram numFeatures (nb or gd)"
-        print "Using defaults instead: 0 20 20 10 90 0 'genre' 'bigram' 0 gd    (alpha = 90/100 = .9)"
+        print "Main function takes 10 arguments: numLabels, numTrainSongs, numTestSongs, trainingIters, alpha, B, artist/genre uni/bi/tri/fourgram numFeatures (nb or gd) (perceptron hinge or log)"
+        print "Using defaults instead: 0 20 20 10 90 0 'genre' 'bigram' 0 gd logistic   (alpha = 90/100 = .9)"
         numLabels = 0 #no preference
         numTrainSongs = 20
         numTestSongs = 20
@@ -52,6 +56,14 @@ def main():
         featureExtractor = 'bigram'
         numFeatures = 0
         useNB = False
+        lossFunc = logisticH
+    
+    #Repeat inputs:
+    if useNB: print "Naive Bayes,", numLabels, "labels,", numTrainSongs, "training songs,", numTestSongs, "testing songs,",
+    else: print "Gradient descent with", lossFunc, "loss function,", numLabels, "labels,", numTrainSongs, "training songs,", numTestSongs,\
+        "testing songs", featureExtractor, "features, use", numFeatures, "features, alpha =", alpha, "B =", B, 
+    if isArtist: print "predict artist."
+    else: print "predict genre."
     
     #LOAD SONGS
     if isArtist:
@@ -65,56 +77,55 @@ def main():
     print "Load examples: ", thisTime - lastTime, ' s'
     lastTime = thisTime
         
-    #FEATURE EXTRACTION   
-    #Classifying by artist:
-    if isArtist:
-        if featureExtractor == 'unigram':
-            trainFeaturesAndLabels = [(extractUnigramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
-            testFeaturesAndLabels = [(extractUnigramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]
-        elif featureExtractor == 'bigram':
-            trainFeaturesAndLabels = [(extractBigramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
-            testFeaturesAndLabels = [(extractBigramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]                         
-        elif featureExtractor == 'trigram':
-            trainFeaturesAndLabels = [(extractTrigramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
-            testFeaturesAndLabels = [(extractTrigramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]                  
+    #FEATURE EXTRACTION
+    #Gradient Descent:
+    #if not useNB:
+    if True:
+        #Classifying by artist:
+        if isArtist:
+            if featureExtractor == 'unigram':
+                trainFeaturesAndLabels = [(extractUnigramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
+                testFeaturesAndLabels = [(extractUnigramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]
+            elif featureExtractor == 'bigram':
+                trainFeaturesAndLabels = [(extractBigramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
+                testFeaturesAndLabels = [(extractBigramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]                         
+            elif featureExtractor == 'trigram':
+                trainFeaturesAndLabels = [(extractTrigramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
+                testFeaturesAndLabels = [(extractTrigramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]                  
+            else: 
+                trainFeaturesAndLabels = [(extractFourgramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
+                testFeaturesAndLabels = [(extractFourgramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]
+        
+        #Classifying by genre:
         else: 
-            trainFeaturesAndLabels = [(extractFourgramFeatures(lyrics), artist) for (lyrics, artist) in trainSongs]
-            testFeaturesAndLabels = [(extractFourgramFeatures(lyrics), artist) for (lyrics, artist) in testSongs]
-    
-    #Classifying by genre:
-    else: 
-        if featureExtractor == 'unigram':
-            trainFeaturesAndLabels = [(extractUnigramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
-            testFeaturesAndLabels = [(extractUnigramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]                         
-        elif featureExtractor == 'bigram':
-            trainFeaturesAndLabels = [(extractBigramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
-            testFeaturesAndLabels = [(extractBigramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]                        
-        elif featureExtractor == 'trigram':
-            trainFeaturesAndLabels = [(extractTrigramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
-            testFeaturesAndLabels = [(extractTrigramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]     
-        else: 
-            trainFeaturesAndLabels = [(extractFourgramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
-            testFeaturesAndLabels = [(extractFourgramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]  
+            if featureExtractor == 'unigram':
+                trainFeaturesAndLabels = [(extractUnigramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
+                testFeaturesAndLabels = [(extractUnigramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]                         
+            elif featureExtractor == 'bigram':
+                trainFeaturesAndLabels = [(extractBigramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
+                testFeaturesAndLabels = [(extractBigramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]                        
+            elif featureExtractor == 'trigram':
+                trainFeaturesAndLabels = [(extractTrigramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
+                testFeaturesAndLabels = [(extractTrigramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]     
+            else: 
+                trainFeaturesAndLabels = [(extractFourgramFeatures(lyrics), genre) for (lyrics, genre) in trainSongs]
+                testFeaturesAndLabels = [(extractFourgramFeatures(lyrics), genre) for (lyrics, genre) in testSongs]         
 
     thisTime = time.clock()
     print "Extract features: ", thisTime - lastTime, ' s'
     lastTime = thisTime
     
-    #FEATURE SELECTION
-    if numFeatures != 0:
-        bestFeatures = featureSelection(trainFeaturesAndLabels, labels, numFeatures)
-        
-        thisTime = time.clock()
-        print "Select features: ", thisTime - lastTime, ' s'
-        lastTime = thisTime
-        
+    #FEATURE SELECTION - only for gradient descent
+    if useNB: bestFeatures = nb.loadDict()
+    elif numFeatures != 0: bestFeatures = featureSelection(trainFeaturesAndLabels, labels, numFeatures) 
+    
+    if bestFeatures:
         #Reduce features in train and test sets to only those in bestFeatures
         trainData = []
         for (features, label) in trainFeaturesAndLabels:
             newFeatures = Counter()
             for feature in features:
                 if feature in bestFeatures: newFeatures[feature] = features[feature]
-            print "Train feature vector length:", len(newFeatures)
             trainData.append((newFeatures, label))
         trainFeaturesAndLabels = trainData
             
@@ -123,7 +134,6 @@ def main():
             newFeatures = Counter()
             for feature in features:
                 if feature in bestFeatures: newFeatures[feature] = features[feature]
-            print "Test feature vector length:", len(newFeatures)
             testData.append((newFeatures, label))
         testFeaturesAndLabels = testData
         
@@ -134,19 +144,19 @@ def main():
     #TRAINING AND CLASSIFICATION
     #Naive Bayes
     if useNB:
-        classifier = nbTrain(trainFeaturesAndLabels, labels, words)
+        classifier = nb.nbTrain(trainFeaturesAndLabels, labels, bestFeatures)
     
     #Gradient Descent    
     else:
-        classifier = trainMultiClassClassifier(trainFeaturesAndLabels, labels, logisticH, trainingIters, alpha, B)
+        classifier = trainMultiClassClassifier(trainFeaturesAndLabels, labels, lossFunc, trainingIters, alpha, B)
     
     thisTime = time.clock()
     print "Train Classifier: ", thisTime - lastTime, ' s'
     lastTime = thisTime    
     
     #Test for errors
-    trainError = classifier.getErrorRate(labels, trainFeaturesAndLabels)
-    testError = classifier.getErrorRate(labels, testFeaturesAndLabels)
+    trainError = classifier.getErrorRate(trainFeaturesAndLabels)
+    testError = classifier.getErrorRate(testFeaturesAndLabels)
    
     thisTime = time.clock()
     print "Error checking: ", thisTime - lastTime, ' s'
